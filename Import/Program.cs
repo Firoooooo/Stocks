51bf4657b3beb64fa0efbc34e5b53749206c4972
Firoooooo <firat.yildiz@gmx.de>
2025-03-31 09:05:33 +0200

    Outsourcing Operation Class

--- Import/Program.cs
+++ Import/Program.cs
@@ -18,8 +18,43 @@ namespace Import
         /// <returns>Task</returns>
         static void Main(string[] _aRGS)
         {
-            Operation oP = new Operation();
-            oP.RunSelectedTask();
+            var rUNNABClasses = Assembly.GetExecutingAssembly().GetTypes()
+                            .Where(T => T.GetCustomAttributes(typeof(RunnableClassAttribute), false).Any())
+                            .Select(T => new
+                            {
+                                Type = T,
+                                Attribute = (RunnableClassAttribute)T.GetCustomAttributes(typeof(RunnableClassAttribute), false).FirstOrDefault()
+                            })
+                            .OrderBy(A => A.Attribute.TransactionNumber)
+                            .ToList();
+            rUNNABClasses.Add(new { Type = typeof(Program), Attribute = new RunnableClassAttribute(rUNNABClasses.Count + 1, "Program beenden") });
+
+            while (true)
+            {
+                Console.Clear();
+                rUNNABClasses.ForEach(T => Console.WriteLine($"{T.Attribute.TransactionNumber} {T.Attribute.Name}"));
+
+                if (int.TryParse(Console.ReadLine(), out int eXCNumber))
+                {
+                    if (rUNNABClasses.Last().Attribute.TransactionNumber == eXCNumber)
+                        break;
+
+                    var sELClass = rUNNABClasses.FirstOrDefault(X => X.Attribute.TransactionNumber == eXCNumber);
+
+                    switch ((OperationTypes)Enum.Parse(typeof(OperationTypes), sELClass.Type.Name))
+                    {
+                        case OperationTypes.DBImportStacks:
+                            DBImportStacks dBImportStacks = new DBImportStacks();
+                            dBImportStacks.Run();
+                            break;
+
+                        case OperationTypes.ClearStocks:
+                            ClearStocks cLEARStocks = new ClearStocks();
+                            cLEARStocks.Run();
+                            break;
+                    }
+                }
+            }
         }
     }
 }
