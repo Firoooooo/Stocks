b8dcd239a1095b62735d4557dc2273db305a0e97
Firoooooo <firat.yildiz@gmx.de>
2025-04-30 08:18:52 +0200

    DB Edits

--- Import/Model/DTOUser.cs
+++ Import/Model/DTOUser.cs
@@ -7,6 +7,7 @@
     {
         public string Username { get; set; }
         public string EMail { get; set; }
+        public string Passowrd { get; set; }
         public string PasswordHash { get; set; }
         public decimal Balance { get; set; }
     }
