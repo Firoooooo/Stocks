055644f669ab4df09de986500827b9e43169a0ee
Firoooooo <firat.yildiz@gmx.de>
2025-04-30 14:38:37 +0200

    sd

--- StocksHTML/Program.cs
+++ StocksHTML/Program.cs
@@ -12,6 +12,8 @@ namespace StocksHTML
             builder.Services.AddRazorComponents()
                 .AddInteractiveServerComponents();
 
+            builder.Services.AddHttpClient();
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
